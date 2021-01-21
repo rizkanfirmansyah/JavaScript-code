081d7837eaac89031f1f54eadb924c40c67950c5
Rizkan Firmansyah <riezkanaprianda@gmail.com>
2021-01-21 07:24:55 +0700

    first commit

---
+++ object.js
@@ -0,0 +1,15 @@
+let user = {
+    name: "Rizkan Firmansyah",
+    age: 18
+  };
+  
+
+  let hasil = Object.key(user)
+  console.log(hasil)
+  
+  // ["Rizkan Firmansyah", "18"]
+  
+  let hasil = Object.entries(user)
+  console.log(hasil)
+  
+  // [["name", "Rizkan Firmansyah"], ["age", "18"]]
